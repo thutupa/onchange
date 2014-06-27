5d1f32899813f21ffaadeee86c73ab6c18c78bfa
Syam Pannala <thutupa@gmail.com>
2014-06-27 00:08:42 -0700

    add mac support and update docs

--- README.txt
+++ README.txt
@@ -1,7 +1,12 @@
-Run a command whenever contents of a set of files change.
+Run a command whenever contents of a file/set of files/files in a directory changes.
 
 Example Usage:
 onchange.py 'shell command to generate list of files you care about' <command to run on change>
 onchange.py 'find . -name \*.c' make new_target
+
+You can also pass in a filename
+onchange.py somefile ...  -> onchange.py 'echo somefile' ...
+And also
+onchage.py somedir ... -> onchange.py 'find somedir -type f' ...
 
 This is essentially a cheap way to do incremental compilation when using non IDE environment
--- onchange.py
+++ onchange.py
@@ -3,15 +3,32 @@
 import subprocess
 import sys
 import time
+import os.path
 
 CHECK_INTERVAL_SECONDS = 1.0
 
 def executeOnChangeAction(action):
     subprocess.Popen(action, shell=True).wait()
 
+MD5COMMAND = None
+def getMd5Command():
+    global MD5COMMAND
+    if not MD5COMMAND:
+        if subprocess.Popen('md5 < /dev/null > /dev/null 2>&1', shell=True).wait() == 0:
+            MD5COMMAND = 'md5'
+        else:
+            MD5COMMAND = 'md5sum -'
+    return MD5COMMAND
+
 def getMd5Sum(filelistgen):
+    if os.path.exists(filelistgen):
+        if os.path.isdir(filelistgen):
+            filelistgen = 'find %s -type f' % filelistgen
+        else:
+            filelistgen = 'echo ' + filelistgen
+
     cmd = subprocess.Popen(
-    	'%s|sort|xargs cat|md5sum -|awk \'{print $1}\'' % filelistgen,
+    	('%s|sort|xargs cat| ' + getMd5Command() + ' |awk \'{print $1}\'') % filelistgen,
 	shell=True, stdout=subprocess.PIPE)
     md5sum = cmd.stdout.read().strip()
     assert cmd.wait() == 0
